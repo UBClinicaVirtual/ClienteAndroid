49f1112a95faa518b02bb4e249e23e5340bb12a7
Fernando Romero <fromero@HCJSM.local>
2018-09-06 12:20:59 -0300

    Se agrega el create , onstart.
    Se agrega en el create,  para pedir el userid mail.
    En onstart,  se fija si ya hay una sesion existente y logueada

--- app/src/main/java/proyectodesarrollo/ub/ubclinicavirtual/LoginActivity.java
+++ app/src/main/java/proyectodesarrollo/ub/ubclinicavirtual/LoginActivity.java
@@ -5,7 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
 
-import android.database.Cursor;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
 
 
 import android.os.Bundle;
@@ -17,10 +26,39 @@ import android.view.View;
  */
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+
+
+    // Build a GoogleSignInClient with the options specified by gso.
+    mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+    @Override
+    public void onStart(){
+     super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
     @Override
