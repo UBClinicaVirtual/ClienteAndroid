9e13a3b00101745b4d056ab8ac943c2f08193bd7
Fernando Romero <fromero@HCJSM.local>
2018-09-06 13:28:48 -0300

    Se agregan los metodos
    Se agregan los metodos SingIng, OnActivityResulta, HandleSigninResult,  Se agrega el listener para el boton.

--- app/src/main/java/proyectodesarrollo/ub/ubclinicavirtual/LoginActivity.java
+++ app/src/main/java/proyectodesarrollo/ub/ubclinicavirtual/LoginActivity.java
@@ -1,6 +1,8 @@
 package proyectodesarrollo.ub.ubclinicavirtual;
 
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -18,7 +20,9 @@ import com.google.android.gms.tasks.Task;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 /**
@@ -28,11 +32,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
     private GoogleSignInClient mGoogleSignInClient;
+    private TextView mStatusTextView;
 
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
 
-
-    // Build a GoogleSignInClient with the options specified by gso.
-    mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
     @Override
@@ -45,6 +49,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
+
+        //Listeners
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
 
 
         // Build a GoogleSignInClient with the options specified by gso.
@@ -61,9 +69,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         updateUI(account);
     }
 
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if (account != null) {
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, account.getDisplayName()));
+            //mStatusTextView.setText(getString(R.string.action_sign_in_short))
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
     @Override
     public void onClick(View v){
+        switch (v.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
 
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
     }
 }
 
